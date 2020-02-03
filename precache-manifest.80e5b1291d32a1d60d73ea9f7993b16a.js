86bd2c67d31e6ba4658670f1ba3f7ed091a58a2f
yuriy_y <yuriy.yurchenko@playtech.com>
2020-02-03 11:26:04 +0200

    Updates

---
+++ precache-manifest.80e5b1291d32a1d60d73ea9f7993b16a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16993296e02c7edd003e0058fa62251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d46249d17e02d83280",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4a7e24d7bd877e24022",
+    "url": "/static/js/2.d514ebb5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d514ebb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d46249d17e02d83280",
+    "url": "/static/js/main.58866b50.chunk.js"
+  },
+  {
+    "revision": "618706733a266e98c9dd",
+    "url": "/static/js/runtime-main.6ff916c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
